4a2b02d593ee028bd4b13778346ce0a071213990
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-12-21 18:24:14 +0530

    Update shayri.py

--- plugins/shayri.py
+++ plugins/shayri.py
@@ -67,7 +67,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/about_me_ankit"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/SHYR_LOVER"
                     ),
                     InlineKeyboardButton(
                         "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/about_me_ankit"
